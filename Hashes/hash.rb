d64e4781bbd07bbebb0648bb225eca3fb45f9218
mtotodev05 <mtotodev05@gmail.com>
2017-12-08 20:32:13 +0300

    Implemented Collection

--- Hashes/hash.rb
+++ Hashes/hash.rb
@@ -1,4 +1,14 @@
 class Hash
+  # A Hash is a collection of key-value pairs like this:
+  # "employee" =>"salary". It is similar to an Array,
+  # except indexing is done via arbitrary keys of any
+  # object type, not an integer index.
 
-
+  $hash_brown ={
+      "topping_1" => "Sour Cream",
+      "topping_2" => "Butter",
+      "topping_3" => "Salt",
+      "topping_4" => "Ketchup"
+  }
+  puts($hash_brown["topping_1"])
 end
