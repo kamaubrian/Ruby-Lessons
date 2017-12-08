d7e96f338c3338dd9c1bd7fa0ea003099d62642f
mtotodev05 <mtotodev05@gmail.com>
2017-12-08 20:47:04 +0300

    new Hash implemented

--- Hashes/hash.rb
+++ Hashes/hash.rb
@@ -1,4 +1,5 @@
 class Hash
+  # Method 1 Creating Hashes
   # A Hash is a collection of key-value pairs like this:
   # "employee" =>"salary". It is similar to an Array,
   # except indexing is done via arbitrary keys of any
@@ -11,4 +12,17 @@ class Hash
       "topping_4" => "Ketchup"
   }
   puts($hash_brown["topping_1"])
+
+  #Method 2 Creating Hashes
+
+  $hash_brown2 = Hash.new()
+  $hash_brown2["topping_4"] = "Jam"
+  $hash_brown2["topping_5"] = "Beef"
+  $hash_brown2["topping_6"] ="Nyama_Choma"
+
+  puts($hash_brown2["topping_6"])
+
+
+
+
 end
