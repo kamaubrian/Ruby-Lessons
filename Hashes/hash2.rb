9725dc4ef0f63cd5a153760a5aa3a8c535e07f90
mtotodev05 <mtotodev05@gmail.com>
2017-12-10 14:59:14 +0300

    Added New Hash

--- Hashes/hash2.rb
+++ Hashes/hash2.rb
@@ -8,5 +8,13 @@ class Hash2
   puts("Are You Male or Female");
   gender = gets.chomp();
 
+  user_details = Hash.new();
+  user_details["first_name"] = first_name;
+  user_details["last_name"] = last_name;
+  user_details["age"] = age;
+  user_details["gender"] = gender;
+
+  puts(user_details);
+
 
 end
