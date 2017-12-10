310343d2ce4bf6d2ad64e349a05207a64546fef1
mtotodev05 <mtotodev05@gmail.com>
2017-12-10 14:26:56 +0300

    Get Chomps method

--- UserInput/chomps.rb
+++ UserInput/chomps.rb
@@ -1,5 +1,10 @@
 class Chomps
-  puts("Enter Your Usernaname")
+  puts("Enter Your Username")
   username = gets.chomp
+
+  puts("Your Username is #{username}")
+  puts("Your Username is "+username.capitalize);
+  puts("Your Username is #{username.upcase}");
+  puts("Your Username is #{username.downcase}");
 
 end
