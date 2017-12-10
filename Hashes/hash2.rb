a02aa041f37485f84fa80486210af1235047a88a
mtotodev05 <mtotodev05@gmail.com>
2017-12-10 14:45:48 +0300

    Added Hash 2

---
+++ Hashes/hash2.rb
@@ -0,0 +1,12 @@
+class Hash2
+  puts("Enter your First Name");
+  first_name = gets.chomp
+  puts("Enter Your Last Name");
+  last_name = gets.chomp();
+  puts("Enter Your Age");
+  age = gets.chomp();
+  puts("Are You Male or Female");
+  gender = gets.chomp();
+
+
+end
