73509c1bf0034162f0148bfb9db5c62dd9e83f86
mtotodev05 <mtotodev05@gmail.com>
2017-12-13 12:41:15 +0300

    For Loop

--- CounterIndex/counter.rb
+++ CounterIndex/counter.rb
@@ -4,5 +4,16 @@ class Counter
   for item in $array_test
     puts(item);
   end
+  for object in $array_test.reverse
+    puts(object)
 
+  end
+##Using a counter.
+#
+#
+    $counter = 1;
+    $array_test.each do | item|
+      puts"#{$counter}: #{item}"
+      $counter+=1
+    end
 end
